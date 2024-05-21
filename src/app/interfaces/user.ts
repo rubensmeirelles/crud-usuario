6e8e2c15240452de37e8d82d7bae04c4a35bb5e7
Rubens <rrubens.meirelles@gmail.com>
2024-05-21 13:34:41 -0300

    Criando a intefrace user

---
+++ src/app/interfaces/user.ts
@@ -0,0 +1,9 @@
+export interface User {
+  nome: string;
+  email: string;
+  sector: string;
+  role: string;
+  firebaseId?: string;
+  healthPlan?: string;
+  dentalPlan?: string;
+}
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
+import { User } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root'
@@ -10,18 +11,18 @@ export class UsersService {
   constructor(private dataBaseService: AngularFirestore) { }
 
   getAllUsers() {
-    return this.dataBaseService.collection('users', user => user.orderBy('name')).valueChanges({idField: 'firebaseId'}) as Observable<any[]>;
+    return this.dataBaseService.collection('users', user => user.orderBy('name')).valueChanges({idField: 'firebaseId'}) as Observable<User[]>;
   }
 
-  addUser(user: any) {
+  addUser(user: User) {
     return this.dataBaseService.collection('users').add(user);
   }
 
-  update(userId: string, user: any) {
-    return this.dataBaseService.collection('users').doc('userId').update(user);
+  update(userId: string, user: User) {
+    return this.dataBaseService.collection('users').doc(userId).update(user);
   }
 
   deleteUser(userId: string) {
-    return this.dataBaseService.collection('users').doc('userId').delete();
+    return this.dataBaseService.collection('users').doc(userId).delete();
   }
 }
